09969c01af4c03816b05166770920194ee93511d
cakejune <tyq444@gmail.com>
2022-08-11 18:08:33 -0400

    commit tree 975216ac3f7ba141d11c64b8086a59385130a942

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Bob",
+    streetAddress: "123 Boo Street",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value)
+{
+    //return {
+   // ...employee,[key]: value,
+  //  };
+    
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const editedEmployee = {...employee};
+    delete editedEmployee["name"];
+    return editedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee["name"];
+    return employee;
+}
+
+
+
